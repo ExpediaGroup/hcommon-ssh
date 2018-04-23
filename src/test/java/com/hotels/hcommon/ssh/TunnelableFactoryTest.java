a14eab487a12ba83f6aa09924c12b95911e2b11b
Daniel del Castillo <ddelcastilloperez@hotels.com>
2018-04-23 13:54:33 +0100

    [initial-code] Restore unit tests

---
+++ src/test/java/com/hotels/hcommon/ssh/TunnelableFactoryTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) 2018 Expedia Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hotels.hcommon.ssh;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+//import static org.mockito.Matchers.eq;
+//import static org.mockito.Mockito.doReturn;
+//import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Method;
+
+//import org.apache.hadoop.hive.conf.HiveConf;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.pastdev.jsch.tunnel.Tunnel;
+import com.pastdev.jsch.tunnel.TunnelConnectionManager;
+
+import com.hotels.hcommon.ssh.tunnel.TunnelConnectionManagerFactory;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TunnelableFactoryTest {
+
+  private static final int SSH_PORT = 22;
+  private static final int REMOTE_PORT = 1024;
+  private static final String REMOTE_HOST = "remote";
+
+  private static interface Connectable extends Tunnelable {
+    public void method();
+  }
+
+  private static class Tunnelled implements Connectable {
+    @Override
+    public void method() {}
+  }
+
+  private @Mock SshSettings sshSettings;
+  private @Mock TunnelConnectionManager tunnelConnectionManager;
+  private @Mock Tunnel tunnel;
+  private @Mock TunnelConnectionManagerFactory tunnelConnectionManagerFactory;
+  private @Mock MethodChecker methodChecker;
+
+  private TunnelableFactory<Tunnelled> tunnelableFactory;
+  private final Tunnelled tunnelled = new Tunnelled();
+
+  @Before
+  public void init() {
+    when(tunnel.getAssignedLocalPort()).thenReturn(SSH_PORT);
+    when(tunnelConnectionManager.getTunnel(anyString(), anyInt())).thenReturn(tunnel);
+    when(tunnelConnectionManagerFactory.create(REMOTE_HOST, REMOTE_PORT)).thenReturn(tunnelConnectionManager);
+    when(tunnelConnectionManagerFactory.getSshSettings()).thenReturn(sshSettings);
+    tunnelableFactory = new TunnelableFactory<>(tunnelConnectionManagerFactory);
+  }
+
+  @Test
+  public void openTunnelUponCreation() throws Exception {
+    tunnelableFactory.wrap(tunnelled, methodChecker, REMOTE_HOST, REMOTE_PORT);
+    verify(tunnelConnectionManager).open();
+  }
+
+  @Test
+  public void ensureTunnelIsOpen() throws Exception {
+    when(methodChecker.isTunnelled(any(Method.class))).thenReturn(true);
+    Connectable proxy = (Connectable) tunnelableFactory.wrap(tunnelled, methodChecker, REMOTE_HOST, REMOTE_PORT);
+    proxy.method();
+    verify(tunnelConnectionManager).ensureOpen();
+    verify(tunnelConnectionManager, never()).close();
+  }
+
+  @Test
+  public void closeTunnel() throws Exception {
+    when(methodChecker.isShutdown(any(Method.class))).thenReturn(true);
+    Connectable proxy = (Connectable) tunnelableFactory.wrap(tunnelled, methodChecker, REMOTE_HOST, REMOTE_PORT);
+    proxy.method();
+    verify(tunnelConnectionManager, never()).ensureOpen();
+    verify(tunnelConnectionManager).close();
+  }
+
+  @Test
+  public void noTunnelConnectivityInteractions() throws Exception {
+    Connectable proxy = (Connectable) tunnelableFactory.wrap(tunnelled, methodChecker, REMOTE_HOST, REMOTE_PORT);
+    proxy.method();
+    verify(tunnelConnectionManager, never()).ensureOpen();
+    verify(tunnelConnectionManager, never()).close();
+  }
+
+}
